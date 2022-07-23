1357a889b430a5dcdc2b014dc23dd7269f9a42ec
thissarthak <sarthakkathpalia@gmail.com>
2022-07-23 10:11:42 +0530

    adding readme

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function validate(){
     let username = document.getElementById("uname").value;
-    let regx = /E00/i;
+    let regx = /^[7-9]\d{9}$/;
     if(regx.test(username)){
-        alert("Valid Username")
+        document.getElementById("lbluser").innerHTML="Valid"
+        document.getElementById("lbluser").style.visibility="visible"
+        document.getElementById("lbluser").style.color='white'
+        // alert("Valid Username")
     }
     else{
-        alert("Invalid Username")
+        // alert("Invalid Username")
+        document.getElementById("lbluser").innerHTML="Invalid"
         document.getElementById("lbluser").style.visibility="visible"
+        document.getElementById("lbluser").style.color='black'
     }
 }
